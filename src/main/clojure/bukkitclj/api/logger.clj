4f2fdc3f0033e911d5ac85e329cdcffcad851977
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-07-24 19:12:19 +0300

    Improve logging wrapper, add trace function

--- src/main/clojure/bukkitclj/api/logger.clj
+++ src/main/clojure/bukkitclj/api/logger.clj
@@ -1,18 +1,22 @@
 (ns bukkitclj.api.logger
-  (:import (eu.mikroskeem.bukkitclj.wrappers LoggerHelper))
+  (:import (eu.mikroskeem.bukkitclj.wrappers LoggerHelper)
+           (org.slf4j.event Level))
   (:gen-class))
 
 (defmacro get-logger []
   `(LoggerHelper/get ~*ns*))
 
+(defmacro trace [^String fmt & args]
+  `(LoggerHelper/log Level/TRACE (get-logger) ~fmt (into-array Object [~@args])))
+
 (defmacro debug [^String fmt & args]
-  `(LoggerHelper/log "debug" (get-logger) ~fmt (into-array Object [~@args])))
+  `(LoggerHelper/log Level/DEBUG (get-logger) ~fmt (into-array Object [~@args])))
 
 (defmacro info [^String fmt & args]
-  `(LoggerHelper/log "info" (get-logger) ~fmt (into-array Object [~@args])))
+  `(LoggerHelper/log Level/INFO (get-logger) ~fmt (into-array Object [~@args])))
 
 (defmacro warn [^String fmt & args]
-  `(LoggerHelper/log "warn" (get-logger) ~fmt (into-array Object [~@args])))
+  `(LoggerHelper/log Level/WARN (get-logger) ~fmt (into-array Object [~@args])))
 
 (defmacro error [^String fmt & args]
-  `(LoggerHelper/log "error" (get-logger) ~fmt (into-array Object [~@args])))
+  `(LoggerHelper/log Level/ERROR (get-logger) ~fmt (into-array Object [~@args])))
